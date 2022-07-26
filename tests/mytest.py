d6af1090efa5364e6fccf880a005dac0c0e12d69
Alon <alonspinner@gmail.com>
2022-07-26 10:55:56 +0300

    mytest is interesting

--- tests/mytest.py
+++ tests/mytest.py
@@ -2,7 +2,7 @@ from bim4loc.binaries.paths import IFC_SIMPLE_PATH, DRONE_PATH, IFC_TEST_PATH
 import open3d as o3d
 import open3d.visualization.rendering as rendering
 from bim4loc.geometry import pose2
-from bim4loc.ifc import converter
+from bim4loc.ifc import converter, ifcObject
 import time
 import numpy as np
 import open3d as o3d
@@ -31,16 +31,19 @@ class visApp():
         self.is_done = True
         return True  # False would cancel the close
 
-    def add_geo(self,geo):
-        self.vis.add_geometry(geo)
-
-
-    
+    def add_geo(self,o : ifcObject):
+        mat = o3d.visualization.rendering.MaterialRecord()
+        mat.shader = "defaultLitTransparency"
+        mat.base_color = np.array([1,0,0,0.5])
+        o.geometry.compute_triangle_normals()
+        self.vis.add_geometry(o.guid, o.geometry, mat)
 
 objects = converter(IFC_TEST_PATH)
 app = visApp()
-threading.Thread(target = app.run()).start()
+threading.Thread(target = app.run).start()
+time.sleep(0.5)
 
 for o in objects:
-    threading.Thread(target = app.add_geo(o.geometry)).start()
+    app.add_geo(o)
+    # threading.Thread(target = app.add_geo, args = (o, )).start()
     time.sleep(1)
