535e9844a22763d3193c619f9cc1f8531eb67766
Alon <alonspinner@gmail.com>
2022-10-25 18:30:11 +0300

    figured out the problem in 07b

--- tests/07a_Mapping_Known_Locations_inverse.py
+++ tests/07a_Mapping_Known_Locations_inverse.py
@@ -5,6 +5,7 @@ from bim4loc.solids import PcdSolid, ifc_converter
 from bim4loc.agents import Drone
 from bim4loc.maps import RayCastingMap
 from bim4loc.sensors.sensors import Lidar
+from bim4loc.geometry.pose2z import compose_s
 from bim4loc.random.utils import p2logodds, logodds2p
 import bim4loc.existance_mapping.filters as filters
 from copy import deepcopy
@@ -28,13 +29,15 @@ for s in solids:
 world = RayCastingMap(constructed_solids)
 
 drone = Drone(pose = np.array([3.0, 3.0, 1.5, 0.0]))
-sensor = Lidar(); sensor.std = 0.1; 
+sensor = Lidar(angles_u = np.linspace(-np.pi, np.pi, 100), angles_v = np.array([0])); 
+sensor.std = 0.1; 
 sensor.piercing = False
 sensor.max_range = 100.0
 drone.mount_sensor(sensor)
 
 simulated_sensor = deepcopy(sensor)
 simulated_sensor.piercing = True
+simulated_sensor.std = 1.0 * sensor.std
 
 simulation_solids = [s.clone() for s in solids]
 simulation = RayCastingMap(simulation_solids)
@@ -66,13 +69,15 @@ visApp.setup_default_camera("simulation")
 time.sleep(1)
 dt = 0.0
 for t,u in enumerate(actions):
-    keyboard.wait('space')
+    # keyboard.wait('space')
     step_start = time.time()
     
     drone.move(u)
     
     z, z_ids, _, z_p = drone.scan(world, project_scan = True)
-    simulated_z, simulated_z_ids, _, _, _ = simulated_sensor.sense(drone.pose, simulation, 10, noisy = False)
+    test_pose = compose_s(drone.pose, np.array([0.1,0.0,0.0,0.0]))
+    simulated_z, simulated_z_ids, _, _, _ = simulated_sensor.sense(test_pose, simulation, 10, noisy = False)
+    # simulated_z, simulated_z_ids, _, _, _ = simulated_sensor.sense(drone.pose, simulation, 10, noisy = False)
 
     filters.approx(logodds_beliefs, z, simulated_z, simulated_z_ids, sensor.std, sensor.max_range)
     simulation.update_solids_beliefs(logodds2p(logodds_beliefs))
--- tests/07b_Mapping_Known_Locations_forward.py
+++ tests/07b_Mapping_Known_Locations_forward.py
@@ -1,10 +1,11 @@
 import numpy as np
 from bim4loc.binaries.paths import IFC_ONLY_WALLS_PATH as IFC_PATH
 from bim4loc.visualizer import VisApp
-from bim4loc.solids import PcdSolid, ifc_converter
+from bim4loc.solids import PcdSolid, ifc_converter, ScanSolid
 from bim4loc.agents import Drone
 from bim4loc.maps import RayCastingMap
 from bim4loc.sensors.sensors import Lidar
+from bim4loc.geometry.pose2z import compose_s, transform_from
 import bim4loc.existance_mapping.filters as filters
 from copy import deepcopy
 import time
@@ -27,7 +28,7 @@ for s in solids:
 world = RayCastingMap(constructed_solids)
 
 drone = Drone(pose = np.array([3.0, 3.0, 1.5, 0.0]))
-sensor = Lidar(angles_u = np.linspace(-np.pi/3, np.pi/3, 36), angles_v = np.array([0])); 
+sensor = Lidar(angles_u = np.linspace(-np.pi, np.pi, 100), angles_v = np.array([0])); 
 sensor.std = 0.1; 
 sensor.piercing = False
 sensor.max_range = 100.0
@@ -35,6 +36,7 @@ drone.mount_sensor(sensor)
 
 simulated_sensor = deepcopy(sensor)
 simulated_sensor.piercing = True
+simulated_sensor.std = 10.0 * sensor.std
 
 simulation_solids = [s.clone() for s in solids]
 simulation = RayCastingMap(simulation_solids)
@@ -53,8 +55,15 @@ visApp.redraw("world")
 visApp.show_axes(True,"world")
 visApp.setup_default_camera("world")
 visApp.add_solid(drone.solid, "world")
-pcd_scan = PcdSolid()
-visApp.add_solid(pcd_scan, "world")
+vis_scan = ScanSolid("scan")
+visApp.add_solid(vis_scan, "world")
+
+simulated_drone = Drone(drone.pose)
+simulated_drone.solid.name = "simulated_drone"
+simulated_drone.solid.material.base_color = np.array([0, 1, 0, 1])
+visApp.add_solid(simulated_drone.solid, "world")
+simulated_vis_scan = ScanSolid("simulated_scan", color = np.array([1.0, 0.0, 0.8]))
+visApp.add_solid(simulated_vis_scan, "world")
 
 #create simulation window
 visApp.add_scene("simulation", "world")
@@ -67,23 +76,32 @@ time.sleep(1)
 dt = 0.0
 start_time = time.time()
 for t,u in enumerate(actions):
-    # keyboard.wait('space')
+    keyboard.wait('space')
     step_start = time.time()
     
     drone.move(u)
-    
-    z, z_ids, _, z_p  = drone.scan(world, project_scan = True)
-    simulated_z, simulated_z_ids, _, _, _ = simulated_sensor.sense(drone.pose, simulation, 10, noisy = False)
+    test_pose = compose_s(drone.pose, np.array([0.01,0.0,0.0,0.0]))
+    simulated_drone.solid.update_geometry(test_pose)
+
+    z, z_ids, _, z_p  = drone.scan(world, project_scan = True, noisy = False)
+    simulated_z, simulated_z_ids, _, _, _ = simulated_sensor.sense(test_pose, simulation, 10, noisy = False)
 
     filters.exact(beliefs, z, simulated_z, simulated_z_ids, 
                     sensor.std, sensor.max_range)
     simulation.update_solids_beliefs(beliefs)
     
-    pcd_scan.update(z_p.T)
+    vis_scan.update(drone.pose[:3], z_p.T)
+    
+    simulated_first_hits = simulated_z[:,0]
+    simulated_drone_p = simulated_sensor.scan_to_points(simulated_first_hits)
+    simulated_world_p = transform_from(test_pose,simulated_drone_p)
+    simulated_vis_scan.update(test_pose[:3], simulated_world_p.T)
 
     [visApp.update_solid(s,"simulation") for s in simulation.solids]
     visApp.update_solid(drone.solid,"world")
-    visApp.update_solid(pcd_scan,"world")
+    visApp.update_solid(simulated_drone.solid,"world")
+    visApp.update_solid(vis_scan, "world")
+    visApp.update_solid(simulated_vis_scan, "world")
     
     visApp.redraw_all_scenes()
     
