7394498fee978d1cfede032d8fbe725feee7f417
Alon <alonspinner@gmail.com>
2023-01-18 16:56:25 +0200

    going home

--- tests/22b_convex_hull.py
+++ tests/22b_convex_hull.py
@@ -1,41 +1,29 @@
 from bim4loc.geometry.minimal_distance import minimal_distance_from_projected_boundry
 from bim4loc.geometry.convex_hull import convex_hull_jarvis as convex_hull
-from bim4loc.geometry.utils import point_in_polygon
 import numpy as np
-import matplotlib.pyplot as plt
-import numpy as np
-from numpy.matlib import repmat
 from bim4loc.binaries.paths import IFC_THREE_WALLS_PATH as IFC_PATH
 from bim4loc.visualizer import VisApp
-from bim4loc.solids import PcdSolid, ifc_converter, ScanSolid, weights2rgb
+from bim4loc.solids import PcdSolid, ifc_converter, ScanSolid
 from bim4loc.agents import Drone
 from bim4loc.maps import RayCastingMap
 from bim4loc.sensors.sensors import Lidar
-from bim4loc.sensors.models import inverse_lidar_model
 from bim4loc.geometry.raycaster import NO_HIT
-import bim4loc.existance_mapping.filters as filters
-from copy import deepcopy
-import time
-import logging
-import keyboard
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
 from bim4loc.geometry.pose2z import transform_from
 import bim4loc.geometry.pose2z as pose2z
 
 solids = [ifc_converter(IFC_PATH)[0]] #list of first element
 world = RayCastingMap(solids)
 
-drone = Drone(pose = np.array([-0.1, 1.0, 0.5, np.pi/6]))
+drone = Drone(pose = np.array([2.0, 2.5, 2.7/2, 0 * np.pi/6]))
 sensor = Lidar(angles_u = np.linspace(-np.pi/4, np.pi/4, 50), 
                angles_v = np.linspace(-np.pi/4, np.pi/4, 50))
-sensor.std = 0.05
 sensor.piercing = False
 sensor.max_range = 10.0
-sensor.p0 = 0.4
 drone.mount_sensor(sensor)
-z, z_ids, z_normals, drone_p = drone.scan(world, n_hits = 1, 
-                                                 project_scan = True, noisy = False)
+
+z, z_ids, z_normals, z_cos_incident, z_n_hits = sensor.sense(drone.pose, world, noisy = False)
+
 #----------------- VISUALIZATION -----------------
 #create world scene
 visApp = VisApp()
@@ -49,19 +37,21 @@ pcd_scan = PcdSolid(shader = "normals")
 visApp.add_solid(pcd_scan, "world")
 line_scan = ScanSolid()
 visApp.add_solid(line_scan,"world")
+sensor_p = sensor.scan_to_points(z)
+drone_p = transform_from(drone.pose, sensor_p)
 pcd_scan.update(drone_p.T, z_normals.reshape(-1,3))
-line_scan.update(drone.pose[:3], drone_p.T)
 visApp.redraw_all_scenes()
 visApp.update_solid(pcd_scan)
-visApp.update_solid(line_scan)
-
-element_world_v = np.asarray(solids[0].geometry.vertices)
-element_uv = pose2z.angle(drone.pose, element_world_v.T).T
+# line_scan.update(drone.pose[:3], drone_p.T)
+# visApp.update_solid(line_scan)
 
 hit_ray_uv = drone.sensor.uv[z_ids != NO_HIT]
-dist_2_boundry = np.zeros(hit_ray_uv.shape[0])
+#-----------------------distance from boundry component-----------------------
+element_world_v = np.asarray(solids[0].geometry.vertices)
+element_uv = pose2z.angle(drone.pose, element_world_v.T).T
+weight_dist_2_boundry = np.zeros(hit_ray_uv.shape[0])
 for i, ray_uv in enumerate(hit_ray_uv):
-    dist_2_boundry[i], _ = minimal_distance_from_projected_boundry(ray_uv, element_uv)
+    weight_dist_2_boundry[i], _ = minimal_distance_from_projected_boundry(ray_uv, element_uv)
         
 
 element_uv_hull = convex_hull(element_uv)
@@ -72,9 +62,35 @@ ax.axis('equal')
 ax.invert_xaxis()
 ax.set_xlabel("yaw, rad"); ax.set_ylabel("pitch, rad")
 ax.set_title("distance from boundry component")
-ax.plot(element_uv_hull_plus[:,0],-element_uv_hull_plus[:,1]) #minus on the ys! for pitch convetion
-sc = ax.scatter(hit_ray_uv[:,0],-hit_ray_uv[:,1],
-                c=dist_2_boundry, s = 50)
+ax.plot(element_uv_hull_plus[:,0],element_uv_hull_plus[:,1])
+sc = ax.scatter(hit_ray_uv[:,0],hit_ray_uv[:,1],
+                c=weight_dist_2_boundry, s = 50)
+fig.colorbar(sc)
+plt.show()
+
+#-----------------------cosine componenet-----------------------
+weight_cos_incident = np.abs(z_cos_incident[z_ids != NO_HIT])
+
+fig, ax = plt.subplots()
+ax.axis('equal')
+ax.invert_xaxis()
+ax.set_xlabel("yaw, rad"); ax.set_ylabel("pitch, rad")
+ax.set_title("cos incident component")
+ax.plot(element_uv_hull_plus[:,0],element_uv_hull_plus[:,1])
+sc = ax.scatter(hit_ray_uv[:,0],hit_ray_uv[:,1],
+                c=weight_cos_incident, s = 50)
+fig.colorbar(sc)
+plt.show()
+
+#-----------------------combined-----------------------
+fig, ax = plt.subplots()
+ax.axis('equal')
+ax.invert_xaxis()
+ax.set_xlabel("yaw, rad"); ax.set_ylabel("pitch, rad")
+ax.set_title("full weight")
+ax.plot(element_uv_hull_plus[:,0],element_uv_hull_plus[:,1])
+sc = ax.scatter(hit_ray_uv[:,0],hit_ray_uv[:,1],
+                c=weight_cos_incident * weight_dist_2_boundry, s = 50)
 fig.colorbar(sc)
 plt.show()
 
